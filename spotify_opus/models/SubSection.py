43fc60b94490fdb534209fde65c23bade4a76c64
James Menzies <james.r.menzies@gmail.com>
2020-12-11 18:05:14 +1100

    sub section added

---
+++ spotify_opus/models/SubSection.py
@@ -0,0 +1,15 @@
+from spotify_opus import db
+from spotify_opus.models.ContextObject import ContextObject
+
+
+class SubSection(ContextObject):
+
+    __tablename__ = "subsections"
+
+    subsection_id = db.Column(db.Integer, db.ForeignKey(
+        "context_objects.context_id"), primary_key=True)
+
+    parent_id = db.Column(db.Integer, db.ForeignKey(
+        "work_components.component_id"), nullable=False)
+
+    number_in_parent = db.Column(db.Integer, nullable=True)
--- spotify_opus/models/WorkComponent.py
+++ spotify_opus/models/WorkComponent.py
@@ -2,16 +2,22 @@ from enum import Enum
 
 from spotify_opus import db
 from spotify_opus.models.ContextObject import ContextObject
-
+from spotify_opus.models.SubSection import SubSection
 
 
 class WorkComponent(ContextObject):
     __tablename__ = "work_components"
+
     component_id = db.Column(db.Integer, db.ForeignKey(
         "context_objects.context_id"), primary_key=True)
+
     scale = db.Column(db.String(), nullable=False)
+
     subsection_id = db.Column(db.Integer, db.ForeignKey(
         "section_names.name_id"), nullable=True)
+
+    children = db.relationship(SubSection, backref="parent", lazy="joined")
+
 
     __mapper_args__ = {
         "polymorphic_identity": "work_component",
