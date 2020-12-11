1b374844ef11f5a9533142f89c388ec85941a070
James Menzies <james.r.menzies@gmail.com>
2020-12-11 13:46:47 +1100

    search registered

--- spotify_opus/controllers/__init__.py
+++ spotify_opus/controllers/__init__.py
@@ -1,7 +1,8 @@
 from typing import List
 from flask import Blueprint
 
+from spotify_opus.controllers.search_controller import search
 
 registerable_controllers: List[Blueprint] = [
-
+    search,
 ]
