b1f3a61c0d2b11611e8b6b8aa046bda0fda6d5ba
James Menzies <james.r.menzies@gmail.com>
2020-12-12 14:06:56 +1100

    View models for media view completed

---
+++ spotify_opus/models/viewmodels.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class SearchItemVM:
+
+    def __init__(self, url: str, image_url: str,
+                 primary_label: str = "Result Name", sec_label: str = None):
+        self.url = url
+        self.image_url = image_url
+        self.primary_label = primary_label
+        self.sec_label = sec_label
+
+
+class CategoryResultVM:
+
+    def __init__(self, name: str):
+        self.name : str = name
+        self.items: List[SearchItemVM] = []
