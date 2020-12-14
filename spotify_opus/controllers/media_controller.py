e82059f5ea9af1ebadd1c0dc5e7b84b91601a880
James Menzies <james.r.menzies@gmail.com>
2020-12-14 13:02:50 +1100

    home page now shows username / correctly identifies when user is logged in

--- spotify_opus/controllers/media_controller.py
+++ spotify_opus/controllers/media_controller.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint, render_template, redirect, url_for, session
 
 from spotify_opus.models.viewmodels import CategoryResultVM, SearchItemVM
@@ -10,15 +11,21 @@ def home_page():
     if 'token' not in session:
         return redirect(url_for("auth.log_in"))
 
+    token = session["token"]
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+
+    if response.status_code != 200:
+        return redirect(url_for("auth.log_in"))
+
+    username = response.json()["display_name"]
+
+
+
+
     results = []
 
-    for i in range(1, 4):
-
-        cat = CategoryResultVM(f"Cat number {i}")
-        for j in range(1, 5):
-            item = SearchItemVM("", "")
-            cat.items.append(item)
-
-        results.append(cat)
-
-    return render_template("media.html", results=results, navbar=True)
+    return render_template("media.html", results=results, username=username, navbar=True)
