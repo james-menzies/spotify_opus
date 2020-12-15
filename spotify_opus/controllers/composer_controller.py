fb194a4f2d435b99f482f42e53a30faed246fe20
James Menzies <james.r.menzies@gmail.com>
2020-12-15 16:20:11 +1100

    composer form controller prepped for model creation

--- spotify_opus/controllers/composer_controller.py
+++ spotify_opus/controllers/composer_controller.py
@@ -1,17 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, abort, redirect, url_for
 
 from spotify_opus.forms import ComposerForm
+from spotify_opus.services.oauth_service import verify_user
 
 composer = Blueprint("composer", __name__, url_prefix="/composer")
 
 
 @composer.route("/", methods=["GET"])
-def get_all_works():
+@verify_user
+def get_all(req_header, user):
     return "yo?"
 
 
 @composer.route("/create", methods=["GET"])
-def create_new_work():
+@verify_user
+def create_new(req_header, user):
     form = ComposerForm()
 
-    return render_template('login.html', form=form)
+    return render_template('composer_edit.html', form=form)
+
+
+@composer.route("/", methods=["POST"])
+@verify_user
+def submit_new(req_header, user):
+    form = ComposerForm(request.form)
+
+    if not form.validate():
+        return redirect(url_for("composer.create_new"))
+
+    Composer
