f7de41842c7912387061b1765cccdc51c8ac0667
James Menzies <james.r.menzies@gmail.com>
2020-12-18 15:57:04 +1100

    data extraction finally working

--- spotify_opus/services/data_extraction.py
+++ spotify_opus/services/data_extraction.py
@@ -1,15 +1,13 @@
-from typing import Tuple, List, Optional, Callable, TypeVar, Set
+from typing import Tuple, List, Optional, Callable, TypeVar, Set, Any
 
 import requests
-from sqlalchemy.orm import aliased
 from werkzeug.exceptions import NotFound
 
 from spotify_opus import db
 from spotify_opus.models.Album import Album
 from spotify_opus.models.Artist import Artist
 from spotify_opus.models.Composer import Composer
-from spotify_opus.models.ContextObject import ContextObject
-from spotify_opus.models.Track import Track
+from spotify_opus.models.Track import Track, association_table
 from spotify_opus.services.oauth_service import verify_user
 
 max_params = {
@@ -27,19 +25,27 @@ def extract_data(composer_id: int):
         print("Composer ID does not exist in database.")
         return
 
-    query_id = composer.artist.external_id
+    query_id = composer.artist.artist_id
     url = f"https://api.spotify.com/v1/artists/{query_id}/albums"
-    albums = extract_items(create_album, url)
+    artists: Set[Artist] = set()
+    albums = extract_items(
+        lambda data: create_album(data, artists), url)
 
-    artists: Set[Artist] = {composer.artist, }
     tracks = []
     url = "https://api.spotify.com/v1/albums/{}/tracks"
 
-    db.session.add_all(albums)
-    db.session.commit()
+    existing_albums = Album.query.all()
+    existing_artists = Artist.query.all()
+
+    albums = [album for album in albums if album not in existing_albums]
+    artists = {artist for artist in artists if artist not in existing_artists}
+
+    db.session.bulk_save_objects(albums)
+    db.session.bulk_save_objects(artists)
+    db.session.flush()
 
     for album in albums:
-        formatted_url = url.format(album.external_id)
+        formatted_url = url.format(album.album_id)
         album_tracks = extract_items(
             lambda data: create_track(data, artists), formatted_url)
 
@@ -47,7 +53,23 @@ def extract_data(composer_id: int):
             track.album_id = album.album_id
         tracks += album_tracks
 
-    db.session.add_all(tracks)
+    db.session.bulk_save_objects(tracks)
+
+    track_artists = []
+    for track in tracks:
+
+        track_artists.append({
+            "track_id": track.track_id,
+            "artist_id": composer.artist.artist_idk
+        })
+
+        for artist in track.artists:
+            track_artists.append({
+                "track_id": track.track_id,
+                "artist_id": artist.artist_id
+            })
+
+    db.session.execute(association_table.insert().values(track_artists))
     db.session.commit()
 
 
@@ -70,7 +92,7 @@ def extract_items(create_func: Callable, url: str) -> List[T]:
 
 @verify_user
 def get_batch(url: str, create_func, req_header: dict,
-              user) -> Tuple[List[Album], Optional[str]]:
+              user) -> Tuple[List[Any], Optional[str]]:
     """Returns a batch of items as well as a url string for the next
     retrieval of items. If this was the last page, the second item in
     the returned Tuple will be None"""
@@ -82,15 +104,17 @@ def get_batch(url: str, create_func, req_header: dict,
     return [create_func(item) for item in items_data], next_url
 
 
-
-
-def create_album(album_data: dict) -> Album:
+def create_album(album_data: dict, artists: Set[Artist]) -> Album:
     album = Album()
     album.name = album_data["name"]
-    album.external_id = album_data["id"]
+    album.album_id = album_data["id"]
     album.image_url = album_data["images"][1]["url"]
     album.album_type = album_data["album_type"]
     album.release_date = album_data["release_date"]
+
+    album_artists = album_data["artists"]
+    new_artists = {create_artist(album_artist) for album_artist in album_artists}
+    artists.update(new_artists)
 
     precision = album_data["release_date_precision"]
     if precision == "year":
@@ -108,19 +132,19 @@ def create_track(
     found if a set is provided."""
 
     track = Track()
-    add_super_attributes(track, track_data)
+    track.name = track_data["name"]
+    track.track_id = track_data["id"]
+    track.track_number = track_data["track_number"]
     track.duration_ms = track_data["duration_ms"]
     track.disc_no = track_data["disc_number"]
     track.explicit = track_data["explicit"]
-    track.artists = [create_artist(data) for data in track_data["artists"]]
-    if artists:
-        artists.union(track.artists)
-
+    artist_ids = [a["id"] for a in track_data["artists"]]
+    track.artists = [artist for artist in artists if artist.artist_id in artist_ids]
     return track
 
 
-def create_artist(artist_data: dict) -> Artist:
+def create_artist(artist_data: str) -> Artist:
     artist = Artist()
-    add_super_attributes(artist, artist_data)
+    artist.name = artist_data["name"]
+    artist.artist_id = artist_data["id"]
     return artist
-
