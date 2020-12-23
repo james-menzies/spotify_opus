d389a1e7009c38af97d8b8fdbe91ca7266ce5515
James Menzies <james.r.menzies@gmail.com>
2020-12-23 14:55:56 +1100

    committing to go back and cherry pick

--- spotify_opus/controllers/performance_controller.py
+++ spotify_opus/controllers/performance_controller.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
+from sqlalchemy.orm import joinedload
 
+from spotify_opus import db
+from spotify_opus.models.Performance import Performance
+from spotify_opus.models.Work import Work
 from spotify_opus.services.oauth_service import verify_user
 
 performance = Blueprint("performance", __name__, url_prefix="/works/<int:work_id>/performances")
@@ -11,4 +15,13 @@ def get_by_work(work_id: int, user, req_header):
     """Get all of the performances of a particular work
     and display to the user."""
 
-    return f"Viewing performances for work_id {work_id}"
+    work = db.session.query(Work).get(work_id)
+
+    query = db.session.query(Performance)
+    query = query.join(Performance.work)
+    query = query.filter(Work.work_id == work_id)
+    query = query.options(joinedload(Performance.album))
+
+    performances = query.all()
+
+
