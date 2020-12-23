218ad2015ad4850ea4fa0a5e6d18a657592420b6
James Menzies <james.r.menzies@gmail.com>
2020-12-23 17:05:44 +1100

    pagination added to works list, satisfying aggregate requirement

--- spotify_opus/controllers/works_controller.py
+++ spotify_opus/controllers/works_controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, abort
+from flask_paginate import get_page_parameter, Pagination
+from sqlalchemy import asc, func
 
 from spotify_opus import db
 from spotify_opus.models.Composer import Composer
@@ -8,20 +10,38 @@ from spotify_opus.services.oauth_service import verify_user
 work = Blueprint(
     "work", __name__, url_prefix='/composers/<int:composer_id>/works')
 
+standard_limit = 30
+
+
 @work.route("/")
 @verify_user
 def get_all(composer_id: int, user, req_header):
     """Gets all works written by the composer ID contained in the
     path of the url."""
+    per_page = 30
+    page = request.args.get(get_page_parameter(), type=int, default=1)
 
-    query = db.session.query(Work)
-    query = query.join(Work.composer)
-    query = query.filter(Composer.composer_id == composer_id)
+    work_query = db.session.query(Work)
+    work_query = work_query.join(Work.composer)
+    work_query = work_query.filter(Composer.composer_id == composer_id)
+    work_query = work_query.order_by(asc(Work.name))
+    work_query = work_query.limit(per_page)
+    work_query = work_query.offset((page - 1) * per_page)
 
-    works = query.all()
+    works = work_query.all()
 
-    return render_template("works.html", works=works)
+    count_query = db.session.query(func.count(Work.work_id))
+    count_query = count_query.join(Work.composer)
+    count_query = count_query.filter(Composer.composer_id == composer_id)
 
+    count = db.session.execute(count_query).scalar()
 
+    composer = db.session.query(Composer).get(composer_id)
 
+    pagination = Pagination(page=page, total=count, per_page=per_page,
+                            css_framework='bootstrap4')
 
+    username = user["display_name"]
+    return render_template("works.html", works=works,
+                           navbar=True, username=username,
+                           composer=composer.name, pagination=pagination)
