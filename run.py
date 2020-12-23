46ade77f16f696ac3b8abfb2a57c89f75817d0cb
James Menzies <james.r.menzies@gmail.com>
2020-12-23 13:10:38 +1100

    migration re added, ssl removed, env tidied

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ from spotify_opus import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    context = "opus.pem", "opus_key.pem"
-    app.run(debug=True, ssl_context=context)
+    app.run(debug=True)
--- spotify_opus/controllers/__init__.py
+++ spotify_opus/controllers/__init__.py
@@ -3,11 +3,9 @@ from flask import Blueprint
 
 from spotify_opus.controllers.album_controller import album
 from spotify_opus.controllers.auth_controller import auth
-from spotify_opus.controllers.media_controller import media
 from spotify_opus.controllers.composer_controller import composer
 
 registerable_controllers: List[Blueprint] = [
-    media,
     composer,
     auth,
     album,
