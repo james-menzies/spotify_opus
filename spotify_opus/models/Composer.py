7ddee8e06d3f9de6a81912142df89b652a7f3b1c
James Menzies <james.r.menzies@gmail.com>
2020-12-24 12:11:54 +1100

    composer edit neatened

--- spotify_opus/models/Composer.py
+++ spotify_opus/models/Composer.py
@@ -5,15 +5,29 @@ from spotify_opus.models.Work import Work
 
 class Composer(db.Model):
     __tablename__ = "composers"
+
     composer_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    birth_year = db.Column(db.Integer, nullable=False)
-    death_year = db.Column(db.Integer, nullable=True)
-    biography = db.Column(db.String(), nullable=True)
-    country = db.Column(db.String(), nullable=False)
+    name = db.Column(db.String, nullable=False,
+                     info={"label": "Name"})
+    birth_year = db.Column(db.Integer, nullable=False,
+                           info={"label": "Year of Birth"})
+
+    death_year = db.Column(db.Integer, nullable=True,
+                           info={"label": "Year of Death"})
+
+    country = db.Column(db.String(), nullable=False,
+                        info={"label": "Country of Origin"})
+
+    biography = db.Column(db.String(), nullable=True,
+                          info={"label": "Biography"})
+
     works = db.relationship(Work, backref="composer")
 
     artist = db.relationship(Artist, backref="composer", uselist=False)
+
+    death_after = db.CheckConstraint("birth_year < death_year", name="death_after")
+
+
 
     def __repr__(self):
         return f"<Composer: {self.name}>"
